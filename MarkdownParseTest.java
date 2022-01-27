69ed203298960b32538eb712bc068b97279a4ec2
Eric Hu <erichu100@outlook.com>
2022-01-26 18:59:43 -0800

    Added testCase for test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,21 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
-//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownPar.3.jar" MarkdownParseTest.java
-//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.ruseTest.java3.jar" org.junit.runner.JUnitCore
+//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void basicTest() throws IOException{
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
 }
