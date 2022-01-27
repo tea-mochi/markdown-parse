5c7ccb3b3c6c4c1dc4f081ab315c3ddc5d1ecf60
Eric Hu <erichu100@outlook.com>
2022-01-26 18:39:32 -0800

    Added MarkdownParseTest
    Uses junit as framework.

---
+++ MarkdownParseTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownPar.3.jar" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.ruseTest.java3.jar" org.junit.runner.JUnitCore
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
